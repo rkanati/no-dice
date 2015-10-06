d24e9d383c4dc79bc25d25177a118a3198b1e5f8
Ben Peggs <b.peggs@googlemail.com>
2015-10-06 22:32:51 +0100

    basic chunk gen

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,104 @@
 #include "glcontext.hpp"
 #include "xwin.hpp"
 
+#include "types.hpp"
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstdint>
 #include <cstring>
 
 #include <stdexcept>
+#include <vector>
 
 #include <GL/gl.h>
 
 namespace nd {
-  void redraw (int width, int height, float t) {
+  struct Vertex {
+    float r, g, b, x, y, z;
+  };
+
+  using Block = u8;
+
+  struct Chunk {
+    Block blocks [16][16][16];
+  };
+
+  class ChunkMesh {
+    std::vector <Vertex> arrays;
+    ChunkMesh (std::vector <Vertex> arrays_in) :
+      arrays (std::move (arrays_in))
+    { }
+
+  public:
+    static ChunkMesh generate (const Chunk&);
+    void draw () const;
+  };
+
+  void ChunkMesh::draw () const {
+    glColorPointer  (3, GL_FLOAT, 24, ((const char*) arrays.data ()) +  0);
+    glVertexPointer (3, GL_FLOAT, 24, ((const char*) arrays.data ()) + 12);
+
+    glDrawArrays (GL_QUADS, 0, arrays.size ());
+  }
+
+  ChunkMesh ChunkMesh::generate (const Chunk& chunk) {
+    std::vector <Vertex> buf;
+    buf.reserve (4 * 3 * 16 * 16 * 16);
+
+    for (int z = 0; z != 15; z++) {
+      for (int y = 0; y != 15; y++) {
+        for (int x = 0; x != 15; x++) {
+          Block current = chunk.blocks [x][y][z];
+
+          if (current != 0) { // face out
+            if (chunk.blocks [x+1][y][z] == 0) { // +x
+              buf.push_back (Vertex { 1, 0, 0, x+1.f, y+0.f, z+0.f });
+              buf.push_back (Vertex { 1, 0, 0, x+1.f, y+1.f, z+0.f });
+              buf.push_back (Vertex { 1, 0, 0, x+1.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 1, 0, 0, x+1.f, y+0.f, z+1.f });
+            }
+            if (chunk.blocks [x][y+1][z] == 0) { // +y
+              buf.push_back (Vertex { 0, 1, 0, x+0.f, y+1.f, z+0.f });
+              buf.push_back (Vertex { 0, 1, 0, x+0.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 0, 1, 0, x+1.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 0, 1, 0, x+1.f, y+1.f, z+0.f });
+            }
+            if (chunk.blocks [x][y][z+1] == 0) { // +z
+              buf.push_back (Vertex { 0, 0, 1, x+0.f, y+0.f, z+1.f });
+              buf.push_back (Vertex { 0, 0, 1, x+1.f, y+0.f, z+1.f });
+              buf.push_back (Vertex { 0, 0, 1, x+1.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 0, 0, 1, x+0.f, y+1.f, z+1.f });
+            }
+          }
+          else if (current == 0) { // face in
+            if (chunk.blocks [x+1][y][z] != 0) { // +x
+              buf.push_back (Vertex { 0, 1, 1, x+1.f, y+0.f, z+0.f });
+              buf.push_back (Vertex { 0, 1, 1, x+1.f, y+0.f, z+1.f });
+              buf.push_back (Vertex { 0, 1, 1, x+1.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 0, 1, 1, x+1.f, y+1.f, z+0.f });
+            }
+            if (chunk.blocks [x][y+1][z] != 0) { // +y
+              buf.push_back (Vertex { 1, 0, 1, x+0.f, y+1.f, z+0.f });
+              buf.push_back (Vertex { 1, 0, 1, x+1.f, y+1.f, z+0.f });
+              buf.push_back (Vertex { 1, 0, 1, x+1.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 1, 0, 1, x+0.f, y+1.f, z+1.f });
+            }
+            if (chunk.blocks [x][y][z+1] != 0) { // +z
+              buf.push_back (Vertex { 1, 1, 0, x+0.f, y+0.f, z+1.f });
+              buf.push_back (Vertex { 1, 1, 0, x+0.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 1, 1, 0, x+1.f, y+1.f, z+1.f });
+              buf.push_back (Vertex { 1, 1, 0, x+1.f, y+0.f, z+1.f });
+            }
+          }
+        }
+      }
+    }
+
+    return ChunkMesh { std::move (buf) };
+  }
+
+  void redraw (int width, int height, float t, const ChunkMesh& mesh) {
     glViewport (0, 0, width, height);
 
     glClearColor (0, 0, 0, 0);
@@ -24,55 +111,16 @@ namespace nd {
     glMatrixMode (GL_PROJECTION);
     glLoadIdentity ();
     float r = float (width) / float (height);
-    float s = 5.0f;
+    float s = 20.0f;
     float x = r * s;
     float y = s;
-    glOrtho (-x, x, -y, y, -10, 10);
+    glOrtho (-x, x, -y, y, -s, s);
 
     glMatrixMode (GL_MODELVIEW);
     glLoadIdentity ();
-    glRotatef (t * 80.f, 0, 0, 1);
-    glTranslatef (3, 0, 0);
     glRotatef (t * 100.0f, 0.5773f, 0.5773f, 0.5773f);
 
-    float ch = 0.9, cm = 0.3, cl = 0.0;
-    glBegin (GL_QUADS);
-      glColor3f (ch, cm, cm);
-      glVertex3f (1, -1, -1);
-      glVertex3f (1,  1, -1);
-      glVertex3f (1,  1,  1);
-      glVertex3f (1, -1,  1);
-
-      glColor3f (cm, cl, cl);
-      glVertex3f (-1, -1, -1);
-      glVertex3f (-1, -1,  1);
-      glVertex3f (-1,  1,  1);
-      glVertex3f (-1,  1, -1);
-
-      glColor3f (cm, ch, cm);
-      glVertex3f (-1, 1, -1);
-      glVertex3f (-1, 1,  1);
-      glVertex3f ( 1, 1,  1);
-      glVertex3f ( 1, 1, -1);
-
-      glColor3f (cl, cm, cl);
-      glVertex3f (-1, -1, -1);
-      glVertex3f ( 1, -1, -1);
-      glVertex3f ( 1, -1,  1);
-      glVertex3f (-1, -1,  1);
-
-      glColor3f (cm, cm, ch);
-      glVertex3f (-1, -1, 1);
-      glVertex3f ( 1, -1, 1);
-      glVertex3f ( 1,  1, 1);
-      glVertex3f (-1,  1, 1);
-
-      glColor3f (cl, cl, cm);
-      glVertex3f (-1, -1, -1);
-      glVertex3f (-1,  1, -1);
-      glVertex3f ( 1,  1, -1);
-      glVertex3f ( 1, -1, -1);
-    glEnd ();
+    mesh.draw ();
   }
 
   class Syncer {
@@ -122,10 +170,27 @@ namespace nd {
   };
 
   extern "C" int main () {
+    Chunk chunk;
+    for (int z = 0; z != 16; z++) {
+      for (int y = 0; y != 16; y++) {
+        for (int x = 0; x != 16; x++) {
+          if (x < 2 || x > 13 || y < 2 || y > 13 || (z / 2) % 2)
+            chunk.blocks [x][y][z] = 0;
+          else
+            chunk.blocks [x][y][z] = 1;
+        }
+      }
+    }
+
+    auto mesh = ChunkMesh::generate (chunk);
+
     auto host = XHost::create ();
     auto gl_ctx = GLContext::establish (host.egl_display (), host.egl_window ());
 
+    glEnable (GL_CULL_FACE);
     glEnable (GL_DEPTH_TEST);
+    glEnableClientState (GL_VERTEX_ARRAY);
+    glEnableClientState (GL_COLOR_ARRAY);
 
     Syncer syncer;
 
@@ -141,7 +206,13 @@ namespace nd {
         // ...
       }
 
-      redraw (host.width (), host.height (), syncer.time () + syncer.time_step * syncer.alpha ());
+      redraw (
+        host.width (),
+        host.height (),
+        syncer.time () + syncer.time_step * syncer.alpha (),
+        mesh
+      );
+
       gl_ctx.flip ();
     }
 
